845dfb4f81ec209e701d35d7349e640417003246
Swati <Swati@10.103.33.135>
2023-04-02 19:31:39 +0530

    Library Management System

---
+++ src/project/ConnectionProvider.java
@@ -0,0 +1,28 @@
+package project;
+import java.sql.*;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+/**
+ *
+ * @author Swati
+ */
+public class ConnectionProvider {
+     public static Connection getCon(){
+    try
+    {
+    Class.forName("com.mysql.jdbc.Driver");
+    Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/lms","root","12345");
+    return con;
+    }
+    catch(Exception e){
+    System.out.println(e);
+    return null;
+    
+    }
+}
+}
